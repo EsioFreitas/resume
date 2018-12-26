4e9ddaf7d9194179f5c2053b9abcb369fa3c50fc
EsioFreitas <esio.gustavo@gmail.com>
2018-12-26 19:56:20 -0200

    Updates

---
+++ precache-manifest.afc3a2a80c5d85eeb44342b859ce6ebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87c56ddb241af3349df8",
+    "url": "/resume/static/css/main.99a6f26a.chunk.css"
+  },
+  {
+    "revision": "87c56ddb241af3349df8",
+    "url": "/resume/static/js/main.87c56ddb.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/resume/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d2697746a5db83ded51e013da182f320",
+    "url": "/resume/index.html"
+  }
+];
