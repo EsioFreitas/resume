85c5dfd0eb0264ad0d03097014e0d8d21424e754
EsioFreitas <esio.gustavo@gmail.com>
2018-12-28 23:15:09 -0200

    Add projects on project-page

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,17 +1,29 @@
 import React, {Component} from 'react'
 
 import Title from '../UI/Title/Title'
-import Project from './Project/Project'
-import projects from '../../helpers/Projects'
+import Project from '../../helpers/Projects'
 
 import './Projects.scss'
 
 
 class Projects extends Component {
+    state ={
+        projects: Project
+    }
     render(){
+        let {projects} = this.state 
         return(
-            <section className='projects'>
+            <section className='projects-page'>
                 <Title title='PROJETOS'/>
+                <div className='projects'>
+                    {
+                        Object.keys(projects).map(key=>(
+                            <div key={key} className='project'>
+                                
+                            </div>
+                        ))
+                    }
+                </div>
                 
             </section>
         );
