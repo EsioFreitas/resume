78d10dc51c97cc298323b9e8f88911a33a6b9b96
EsioFreitas <esio.gustavo@gmail.com>
2018-12-29 09:28:19 -0200

    Add Educaion page

---
+++ src/Components/Education/Education.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Title from '../UI/Title/Title'
+
+import './Education.scss'
+
+const education = () =>(
+    <section className='education'>
+        <Title title="Educação"/>
+    </section>
+);
+
+export default education;
