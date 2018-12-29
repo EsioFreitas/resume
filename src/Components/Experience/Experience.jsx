38ed3c9674b04135f2f8a5d0eccba3685268bb71
EsioFreitas <esio.gustavo@gmail.com>
2018-12-29 09:30:04 -0200

    Add Experience page

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Title from '../UI/Title/Title'
+
+import './Experience.scss'
+
+const experience = () =>(
+    <section className='experience'>
+        <Title title="EXPERIÊNCIA"/>
+    </section>
+);
+
+export default experience;
