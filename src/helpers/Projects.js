9820d2b4efc8ce006a0e90712cb03a5439c00f23
EsioFreitas <esio.gustavo@gmail.com>
2018-12-28 22:02:24 -0200

    Add Projects helper

---
+++ src/helpers/Projects.js
@@ -0,0 +1,34 @@
+const projects = {
+    nuntium:{
+      title: 'Nuntium',
+      subtitle: 'Ruby on Rails',
+      whats:'Rede social de blogers',
+      technologies: ['Ruby on Rails', 'Bootstrap'],
+      description: 'Um texto bem legal do projeto aqui ta',
+      repository: 'https://github.com/EsioFreitas/Nuntium',
+      hover: false,
+      photo: foto
+    },
+    kalkuli:{
+      title: 'Kalkuli',
+      subtitle: 'React + Flask',
+      whats:'Controle fiscal e financeiro',
+      technologies: ['React', 'Flask', 'Sass', 'Redux'],
+      description: 'Um texto bem legal do projeto aqui ta',
+      repository: 'https://github.com/Kalkuli',
+      hover: false,
+      photo: foto
+    },
+    portfolio:{
+      title: 'Portfólio',
+      subtitle: 'React',
+      whats:'Meu portfólio',
+      technologies: ['React', 'Sass'],
+      description: 'Um texto bem legal do projeto aqui ta',
+      repository: 'https://github.com/EsioFreitas/portfolio',
+      hover: false,
+      photo: portfolioCapa
+    }
+  }
+
+  export default projects;
