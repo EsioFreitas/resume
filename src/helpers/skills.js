369d4fba075ac52b01783cf258fb43ca789cbfbe
EsioFreitas <esio.gustavo@gmail.com>
2018-12-31 21:10:00 -0200

    Add skills helper

---
+++ src/helpers/skills.js
@@ -0,0 +1,16 @@
+const skills = {
+    tecnologias: {
+        title: 'Conhecimento técnico',
+        option: ['Vanilla JS', 'React JS', 'Python', 'Flask', 'CSS', 'HTML', 'Ruby on Rails', 'C++', 'Git', 'Sass']
+    },
+    Soft:{
+        title: 'Soft Skills',
+        option: ['Liderança', 'Proatividade', 'Criatividade', 'Trabalho em equipe', 'Comunicativo']
+    },
+    interesses: {
+        title: 'Interresses',
+        option: ['Engenharia de Software', 'Metodologia Ágil', 'Gamificação', 'Empreededorismo', 'Desenvolvimento Web', 'UX/UI', 'Educação']
+    }
+}
+
+export default skills;
