0ee3b148e53dae344493bc7172d16d70019cd9b2
EsioFreitas <esio.gustavo@gmail.com>
2019-01-01 09:46:58 -0200

    Style footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
+import linkedinImg from '../../Assets/icons/linkedin.svg'
+import githubImg from '../../Assets/icons/github.svg'
+
 import './Footer.scss'
 
 const footer = () => (
     <footer>
         <p>© 2019 Esio Freitas</p>
         <div className='footer_imgs'>
-            
+            <a href='https://github.com/EsioFreitas' target="_blank">
+                <img src={githubImg} alt='GitHub'/>
+            </a>
+            <a href='https://www.linkedin.com/in/esiofreitas/' target="_blank">
+                <img src={linkedinImg} alt='Linkedin'/>
+            </a>
         </div>    
     </footer>
 );
