ce685d916e0a6ed9c4525a5d3908e032d9817dfe
EsioFreitas <esio.gustavo@gmail.com>
2018-12-31 22:14:01 -0200

    Style footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import './Footer.scss'
+
 const footer = () => (
     <footer>
+        <p>© 2019 Esio Freitas</p>
+        <div className='footer_imgs'>
+            
+        </div>    
     </footer>
 );
 
